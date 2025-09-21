2b6f2806514a3b9b1adcb6037fc47299b28296ad
Husar27 <acode2025@proton.me>
2025-09-21 22:10:02 +0200

    Delete php-backend directory

--- php-backend/users.sql
+++
@@ -1,7 +0,0 @@
--- users.sql
--- Uruchom w phpMyAdmin lub konsoli MySQL, aby utworzyć tabelę użytkowników
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    login VARCHAR(64) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
